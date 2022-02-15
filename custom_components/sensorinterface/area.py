7ac8390ef412f19b5bc4c422d2619c61387b1b82
chof747 <chof@gmx.at>
2022-02-15 20:38:02 +0000

    refactoring area service to be ready for both directions
    - moved preparation of sensor list to own routine
    - moved building of results to own routine

--- custom_components/sensorinterface/area.py
+++ custom_components/sensorinterface/area.py
@@ -1,6 +1,7 @@
 from array import array
 import logging
 from operator import attrgetter
+from xml.dom.minidom import Entity
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import area_registry, entity_registry
 
@@ -13,7 +14,7 @@ async def get_all_areas(hass: HomeAssistant) -> array:
     return result
 
 
-async def get_next_sensor_in_area(hass: HomeAssistant, area: str, current: str) -> dict:
+async def prepare_sensor_list(hass: HomeAssistant, area: str) -> list:
     eids = hass.states.async_entity_ids("sensor")
     ent_reg = await entity_registry.async_get_registry(hass)
     sensors = []
@@ -25,9 +26,28 @@ async def get_next_sensor_in_area(hass: HomeAssistant, area: str, current: str) 
             sensors.append(entity)
 
     if len(sensors) > 0:
-        sorted_sensors = sorted(sensors, key=attrgetter("device_class", "name"))
-        selected = sorted_sensors[0]
+        return sorted(sensors, key=attrgetter("device_class", "name"))
+    else:
+        return None
 
+
+def build_result(hass: HomeAssistant, selected: Entity) -> dict:
+    state = hass.states.get(selected.entity_id)
+    return {
+        "id": selected.entity_id,
+        "name": selected.name,
+        "type": selected.device_class,
+        "value": state.state,
+        "unit": selected.unit_of_measurement,
+    }
+
+
+async def get_next_sensor_in_area(hass: HomeAssistant, area: str, current: str) -> dict:
+    sorted_sensors = await prepare_sensor_list(hass, area)
+
+    if sorted_sensors != None:
+
+        selected = sorted_sensors[0]
         for i in range(0, len(sorted_sensors)):
             if current == sorted_sensors[i].entity_id:
                 if (i + 1) < len(sorted_sensors):
@@ -37,15 +57,7 @@ async def get_next_sensor_in_area(hass: HomeAssistant, area: str, current: str) 
                     selected = sorted_sensors[0]
                     break
 
-        state = hass.states.get(selected.entity_id)
-
-        return {
-            "id": selected.entity_id,
-            "name": selected.name,
-            "type": selected.device_class,
-            "value": state.state,
-            "unit": selected.unit_of_measurement,
-        }
+        return build_result(hass, selected)
     else:
         return {}
 
