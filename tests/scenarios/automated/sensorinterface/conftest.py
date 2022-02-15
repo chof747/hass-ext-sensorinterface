64160ddc5b1979f8c9d621fa15d416dcc4b6f87a
chof747 <chof@gmx.at>
2022-02-15 10:28:23 +0000

    cleanup testing setup

--- tests/scenarios/automated/sensorinterface/conftest.py
+++ tests/scenarios/automated/sensorinterface/conftest.py
@@ -31,11 +31,7 @@ def hassendpoint(sockets, MQTT):
 
         while not mqtt_fixture.isHassOnline and not timeout(startTime):
             sleep(10)
-        if not timeout(startTime):
-            registerSensors()
-            return True
-        else:
-            return False
+        return not timeout(startTime)
 
 
 def checkHomeAssIsRunningLocally():
@@ -52,22 +48,3 @@ def timeout(startTime: float):
         LOGGER.error("Timeout starting Home Assistant")
         return True
     return False
-
-
-def registerSensors():
-    mqtt_fixture = MQTT()
-    mqtt_devices = [
-        f
-        for f in os.listdir(DEVICE_DISCOVERY_PATH)
-        if (os.path.splitext(f)[1][1:] == "json")
-    ]
-
-    for d in mqtt_devices:
-        df = open(os.path.join(DEVICE_DISCOVERY_PATH, d), "r", encoding="utf-8")
-        device = df.read()
-        df.close()
-
-        did = os.path.splitext(d)[0]
-        mqtt_fixture.registerSensor(did, device)
-
-    sleep(2)
